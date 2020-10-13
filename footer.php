057f49598b03738429d83ece10238356a93a0e64
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-10-13 13:19:32 +0200

    Ajout du footer

--- footer.php
+++ footer.php
@@ -8,85 +8,59 @@
  */
 ?>
 
-	</div><!-- #content -->
+	<!-- </div>#content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo"><!-- création d'un contenair flexbox footer -->
+	<footer role="contentinfo"><!-- création d'un contenair flexbox footer -->
 
-	
-		<div class="conteneur_contact_newsletter">
+	<div class="colonne1">
 
-			<div class="contact_bloc">
+		<img class="img-MRL" src="<?php echo get_stylesheet_directory_uri() . "/Images/MRL_image.png" ?>" alt="logo MRL">
 
-				<img class="img-MRL" src="<?php echo get_stylesheet_directory_uri() . "/Images/MRL_image.png" ?>" alt="logo MRL">
+				<h3>CONTACTS</h3>
 
-				<h1 id="titre_contacts">CONTACTS</h1>
+					<div class="reseaux">
+						<a href="https://www.facebook.com/maurice.radio.libre/" class="facebook"></a>
+						<a href="https://twitter.com/Maurice_Radio" class="twitter"></a>
+						<a href="https://www.instagram.com/maurice_radio_libre/?hl=fr" class=" instagram"></a>
+						<a href="https://www.youtube.com/user/mauriceradiolibrecaa" class="youtube"></a>
+						<a href="https://soundcloud.com/maurice-radio-libre" class=" soundcloud"></a>
+					</div>
 
-					<p id="reseaux_footer">
-						<a href="https://www.facebook.com/maurice.radio.libre/" class="icones_social facebook_footer" alt="facebook" >facebook</a>
-						<a href="https://twitter.com/Maurice_Radio" class="icones_social twitter_footer" alt="twitter" >twitter</a>
-						<a href="https://www.instagram.com/maurice_radio_libre/?hl=fr" class="icones_social instagram_footer" alt="instagram">instagram</a>
-						<a href="https://www.youtube.com/user/mauriceradiolibrecaa" class="icones_social youtube_footer" alt="youtube">youtube</a>
-						<a href="https://soundcloud.com/maurice-radio-libre" class="icones_social soundcloud_footer" alt="soundcloud">soundcloud</a>
-																	
-					</p>
-			</div>
-		
+				<h3>NEWSLETTER</h3>
 
-			<div class="newsletter_bloc">
+					<p>Enquiers-toi de l'actu de Maurice Radio Libre par mail et gratos en plus! Inscris ton email ici!</p>
 
-				<h1 id="titre_newsletter">NEWSLETTER</h1>
-
-					<p id="phrase_newsletter">Enquiers-toi de l'actu de Maurice Radio Libre par mail et gratos en plus! Inscris ton email ici!</p>
+					<div class = "newsletter-box">
 
 						<form method="post"	action="">
-						<input type="email" class="email" placeholder="Votre Adresse E-mail" />
-						<input type="submit" id="bouton" value="S'abonner" />
-					</form>
-			
-			</div>
+							<input type="email" placeholder="Votre Adresse E-mail" />
+							<input type="submit" value="S'abonner" id="submitNewsletterBtn"/>
+						</form>
 
-		
-		</div>
+					</div>
 
+	</div>
+	<div class="colonne2">
+        <?php include 'wp-content/plugins/MRL-Accueil/templates/partenaires-front.php';?>
+	</div>
 
-		<div class="partenaires">
+	<div class="colonne3">
 
-				<h1 id="titre_partenaires">NOS PARTENAIRES</h1>
+		<h3>INFORMATIONS</h3>
 
-				<p id="liens_partenaires">
-						<a href="https://www.culture.gouv.fr/" class="icones_partenaires image1_footer" alt="République française" >République française</a>
-						<a href="https://www.deezer.com/fr/" class="icones_partenaires image2_footer" alt="deezer" >deezer</a>
-						<a href="https://www.6play.fr/m6music/programme-tv" class="icones_partenaires image3_footer" alt="M6 Music Hits">M6 Music Hits</a>
-				</p>
-							
-		</div>
+		<p class="liens_informations">
+			<a href="">À PROPOS DE MRL</a><br>
+			<a href="">À PROPOS DU CAA</a><br>
+			<a href="">MENTIONS LÉGALES - COOKIE</a><br>
+			<a href="">CRÉDIT</a><br>
+		</p>
+	</div>
 
-		<div class="informations">
+		<p class="copyright">Copyright © <?=date('Y')?> - Maurice Radio Libre. Tous les droits réservés. Toutes reproductions interdites.</p>
 
-				<h1 id="titre_informations">INFORMATIONS</h1>
-
-				<p id="liens_site">
-					<a href="" class="redirection MRL" alt="A propos de MRL" >À PROPOS DE MRL</a>
-					<a href="" class="redirection CAA" alt="A propos du CAA" >À PROPOS DU CAA</a>
-					<a href="" class="redirection Mentions" alt="Mentions légales - Cookie" >MENTIONS LÉGALES - COOKIE</a>
-					<a href="" class="redirection Crédit" alt="Crédit" >CRÉDIT</a>
-				</p>
-
-		</div>
-	
-		<div class="copyright">
-
-			<p id="texte_copyright">Copyright © 2020 - Maurice Radio Libre. Tous les droits réservés. Toutes reproductions interdites.</p>
-
-		</div>
-
-		
-		
 	</footer><!-- #colophon -->
 
-
-
-</div><!-- #page -->
+<!-- </div>#page -->
 
 <?php wp_footer(); ?>
 
