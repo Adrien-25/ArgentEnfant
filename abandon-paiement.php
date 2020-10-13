41e29b541c0d719e21f11ef4e159d236656277b8
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2020-10-13 13:13:14 +0200

    Ajout du template d'abandon de paiement

---
+++ abandon-paiement.php
@@ -0,0 +1,15 @@
+<?php
+/* Template Name: Abandon Paypal Template */
+
+ get_header();  ?>
+<link rel="stylesheet" href="/wp-content/themes/ArgentEnfant/abandon_paypal.css">
+
+
+
+<div>
+    <div class="content-page">
+    <?php get_template_part( 'content', 'page' ); ?>
+    </div>
+
+
+<?php get_footer(); ?>
